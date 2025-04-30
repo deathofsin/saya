807f878d51babb702b9c40a6be7788f25b7e4fae
Sin <aestheticsin@protonmail.com>
2025-04-30 14:17:33 +0200

    update and new commands!!! OwO

--- saya bot/main.py
+++ saya bot/main.py
@@ -5,8 +5,8 @@ from modules.scraper import *
 from modules.ping import *
 from modules.afk import *
 from modules.purge import *
-
-
+from modules.uwufy import *
+from modules.globalBan import *
 
 client = TelegramClient(session="Saya",
     api_id=config.API_ID,
@@ -18,5 +18,7 @@ register_ping(client)
 register_afk(client)
 register_scraper(client)
 register_purge(client)
+register_uwuify(client)
+register_gban(client)
 client.start()
 client.run_until_disconnected()
