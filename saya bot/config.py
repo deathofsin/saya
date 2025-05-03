95b85b8d4e3d19ce667b5b61c52e0c8d9afe6df8
Sin <aestheticsin@protonmail.com>
2025-05-03 12:45:42 +0200

    Update config.py

--- saya bot/config.py
+++ saya bot/config.py
@@ -1,3 +1,3 @@
-API_ID = 2899 #insert your api ID
-API_HASH = "36722c72256a24c1225de00eb6a1ca74" #insert your api hash
-PHONE_NUMBER = "+447300653497" #insert phone number
+API_ID =  #insert your api ID
+API_HASH = "" #insert your api hash
+PHONE_NUMBER = "" #insert phone number
